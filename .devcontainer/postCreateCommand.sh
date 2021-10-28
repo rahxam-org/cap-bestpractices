fb14b6392b73f35f2df7fb91c6c303845f03cbe9
rahxam <segelmaex@sc-schwielochsee.de>
2021-10-28 11:56:22 +0000

    docs(contributing.md): update Contribution Guide

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-
+# Stop script immediately in case of a failure and show the the result in realtime
 set -ex
 
+npm install -g @sap/cds-dk mbt yo standard sqlite3
 npm i
+
+# Istall and initialize pre commit
 pip install pre-commit
 pre-commit install
 pre-commit
 
+# Configure the GIT merge strategy
 git config --global pull.rebase false
